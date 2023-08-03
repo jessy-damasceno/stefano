b5bdb47dfec6150eda452e5d121426f8a738f724
Jessy Damasceno <jessy_bass@hotmail.com>
2023-08-03 01:57:39 -0300

    feat: editRequests

---
+++ typescript-version/src/requests/contacts.request.ts
@@ -0,0 +1,118 @@
+import axios from "axios";
+import 'dotenv/config';
+
+export interface IContact {
+  id: number;
+  userId: number;
+  contactName: string;
+  email: string;
+}
+
+interface INewContact {
+  contactName: string;
+  email: string;
+}
+
+const HOST = process.env.HOST ?? 'http://localhost:3001';
+
+export const getContactsRequest = async (token: string) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.get(
+    `${HOST}/contact`,
+    config
+  ).then((response) => {
+    return {
+      status: response.status,
+      message: null,
+      contacts: response.data as IContact[],
+    };
+  }).catch((error) => {
+    return {
+      status: error.response.status,
+      message: error.response.data.message,
+      contacts: [],
+    };
+  });
+
+  return response;
+};
+
+export const addNewContact = async (token: string, payload: INewContact) => {
+  const { contactName, email } = payload;
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.post(
+    `${HOST}/contact`,
+    { contactName, email },
+    config,
+  ).then((response) => {
+    return {
+      status: response.status,
+      message: null,
+      contact: response.data as IContact,
+    };
+  }).catch((error) => {
+    return {
+      status: error.response.status,
+      message: error.response.data.message,
+      contact: null,
+    };
+  });
+
+  return response;
+};
+
+export const editContactRequest = async (token: string, id: string | number, payload: INewContact) => {
+  const { contactName, email } = payload;
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.put(
+    `${HOST}/contact/${id}`,
+    { contactName, email },
+    config,
+  ).then((response) => {
+    return {
+      status: response.status,
+      message: 'Success',
+      contact: response.data as IContact,
+    };
+  }).catch((error) => {
+    return {
+      status: error.response.status,
+      message: error.response.data.message,
+      contact: null,
+    };
+  });
+
+  return response;
+};
+
+export const removeContactRequest = async (token: string, id: string | number) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.delete(
+    `${HOST}/contact/${id}`,
+    config,
+  ).then((response) => {
+    return {
+      status: response.status,
+      message: response.data.message,
+    };
+  }).catch((error) => {
+    return {
+      status: error.response.status,
+      message: error.response.data.message,
+    };
+  });
+
+  return response;
+};
--- typescript-version/src/views/dashboard/Table.tsx
+++ typescript-version/src/views/dashboard/Table.tsx
@@ -1,7 +1,9 @@
+import { useState, useEffect, ChangeEvent } from 'react'
+
 // ** MUI Imports
 import Box from '@mui/material/Box'
+import CardContent from '@mui/material/CardContent'
 import Card from '@mui/material/Card'
-import Chip from '@mui/material/Chip'
 import Table from '@mui/material/Table'
 import TableRow from '@mui/material/TableRow'
 import TableHead from '@mui/material/TableHead'
@@ -9,148 +11,186 @@ import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import Typography from '@mui/material/Typography'
 import TableContainer from '@mui/material/TableContainer'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 
-// ** Types Imports
-import { ThemeColor } from 'src/@core/layouts/types'
+import { getContactsRequest, addNewContact, removeContactRequest, IContact, editContactRequest } from 'src/requests/contacts.request'
+import { IconButton } from '@material-ui/core'
 
-interface RowType {
-  age: number
-  name: string
-  date: string
+interface State {
+  contactName: string
   email: string
-  salary: string
-  status: string
-  designation: string
-}
-
-interface StatusObj {
-  [key: string]: {
-    color: ThemeColor
-  }
-}
-
-const rows: RowType[] = [
-  {
-    age: 27,
-    status: 'current',
-    date: '09/27/2018',
-    name: 'Sally Quinn',
-    salary: '$19586.23',
-    email: 'eebsworth2m@sbwire.com',
-    designation: 'Human Resources Assistant'
-  },
-  {
-    age: 61,
-    date: '09/23/2016',
-    salary: '$23896.35',
-    status: 'professional',
-    name: 'Margaret Bowers',
-    email: 'kocrevy0@thetimes.co.uk',
-    designation: 'Nuclear Power Engineer'
-  },
-  {
-    age: 59,
-    date: '10/15/2017',
-    name: 'Minnie Roy',
-    status: 'rejected',
-    salary: '$18991.67',
-    email: 'ediehn6@163.com',
-    designation: 'Environmental Specialist'
-  },
-  {
-    age: 30,
-    date: '06/12/2018',
-    status: 'resigned',
-    salary: '$19252.12',
-    name: 'Ralph Leonard',
-    email: 'dfalloona@ifeng.com',
-    designation: 'Sales Representative'
-  },
-  {
-    age: 66,
-    status: 'applied',
-    date: '03/24/2018',
-    salary: '$13076.28',
-    name: 'Annie Martin',
-    designation: 'Operator',
-    email: 'sganderton2@tuttocitta.it'
-  },
-  {
-    age: 33,
-    date: '08/25/2017',
-    salary: '$10909.52',
-    name: 'Adeline Day',
-    status: 'professional',
-    email: 'hnisius4@gnu.org',
-    designation: 'Senior Cost Accountant'
-  },
-  {
-    age: 61,
-    status: 'current',
-    date: '06/01/2017',
-    salary: '$17803.80',
-    name: 'Lora Jackson',
-    designation: 'Geologist',
-    email: 'ghoneywood5@narod.ru'
-  },
-  {
-    age: 22,
-    date: '12/03/2017',
-    salary: '$12336.17',
-    name: 'Rodney Sharp',
-    status: 'professional',
-    designation: 'Cost Accountant',
-    email: 'dcrossman3@google.co.jp'
-  }
-]
-
-const statusObj: StatusObj = {
-  applied: { color: 'info' },
-  rejected: { color: 'error' },
-  current: { color: 'primary' },
-  resigned: { color: 'warning' },
-  professional: { color: 'success' }
+  id: number | null
 }
 
 const DashboardTable = () => {
+  // ** State
+  const [isEdit, setIsEdit] = useState<boolean>(false)
+  const [token, setToken] = useState<string>('')
+  const [contacts, setContacts] = useState<IContact[]>([])
+  const [values, setValues] = useState<State>({
+    contactName: '',
+    email: '',
+    id: null,
+  })
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const tokenToSave = localStorage.getItem('token') as string
+      setToken(tokenToSave)
+      const response = await getContactsRequest(tokenToSave)
+
+      setContacts(response.contacts)
+    }
+
+    getContacts()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const resetFields = () => {
+    setIsEdit(false);
+    setValues({
+      contactName: '',
+      email: '',
+      id: null,
+    });
+  }
+
+  const handleChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    const newValues = { ...values, [prop]: event.target.value }
+
+    setValues(newValues)
+  }
+
+  const handleClick = async () => {
+    const response = await addNewContact(token, {
+      contactName: values.contactName,
+      email: values.email
+    })
+
+    console.log(response.status)
+
+    if (response.status === 201) {
+      setContacts([...contacts, response.contact as IContact])
+    }
+
+    console.log(contacts)
+
+    alert(response.message)
+    resetFields()
+  }
+
+  const removeContact = async (id: string | number) => {
+    const response = await removeContactRequest(token, id)
+
+    if (response.status === 200) {
+      const newContacts = contacts.filter(contact => contact.id != id)
+      setContacts(newContacts)
+    }
+
+    alert(response.message)
+  }
+
+  const startEdit = (id: number) => {
+    const contactToEdit = contacts.find(contact => contact.id === id) as IContact;
+
+    setValues({
+      contactName: contactToEdit.contactName,
+      email: contactToEdit.email,
+      id
+    })
+
+    setIsEdit(true)
+  };
+
+  const editContact = async () => {
+    const { contactName, email, id } = values;
+    const response = await editContactRequest(token, id as number, {
+      contactName,
+      email
+    });
+
+    if (response.status === 200) {
+      const newContacts = contacts.map(contact => contact.id === id? {...contact, contactName, email } : contact)
+      setContacts(newContacts);
+    };
+
+    alert(response.message);
+    setIsEdit(false);
+  };
+
+  console.log(isEdit)
+
   return (
     <Card>
+      <CardContent>
+        <form onSubmit={e => e.preventDefault()}>
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label='Contact Name'
+                placeholder='Leonard Carter'
+                value={values.contactName}
+                onChange={handleChange('contactName')}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                type='email'
+                label='Email'
+                placeholder='carterleonard@gmail.com'
+                value={values.email}
+                onChange={handleChange('email')}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Button fullWidth onClick={() => !isEdit ? handleClick() : editContact()} variant='contained' size='large'>
+                {!isEdit ? 'ADD NEW CONTACT' : 'EDIT CONTACT'}
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <Button fullWidth onClick={resetFields} color='error' variant='outlined' size='large'>
+                CANCEL
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </CardContent>
+
       <TableContainer>
         <Table sx={{ minWidth: 800 }} aria-label='table in dashboard'>
           <TableHead>
             <TableRow>
-              <TableCell>Name</TableCell>
+              <TableCell>ID</TableCell>
+              <TableCell>Contact Name</TableCell>
               <TableCell>Email</TableCell>
-              <TableCell>Date</TableCell>
-              <TableCell>Salary</TableCell>
-              <TableCell>Age</TableCell>
-              <TableCell>Status</TableCell>
+              <TableCell>Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row: RowType) => (
-              <TableRow hover key={row.name} sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
+            {contacts.map((contact: IContact) => (
+              <TableRow hover key={contact.id} sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
+                <TableCell>{contact.id}</TableCell>
                 <TableCell sx={{ py: theme => `${theme.spacing(0.5)} !important` }}>
                   <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography sx={{ fontWeight: 500, fontSize: '0.875rem !important' }}>{row.name}</Typography>
-                    <Typography variant='caption'>{row.designation}</Typography>
+                    <Typography sx={{ fontWeight: 500, fontSize: '0.875rem !important' }}>
+                      {contact.contactName}
+                    </Typography>
                   </Box>
                 </TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.date}</TableCell>
-                <TableCell>{row.salary}</TableCell>
-                <TableCell>{row.age}</TableCell>
+                <TableCell>{contact.email}</TableCell>
                 <TableCell>
-                  <Chip
-                    label={row.status}
-                    color={statusObj[row.status].color}
-                    sx={{
-                      height: 24,
-                      fontSize: '0.75rem',
-                      textTransform: 'capitalize',
-                      '& .MuiChip-label': { fontWeight: 500 }
-                    }}
-                  />
+                  <IconButton onClick={() => startEdit(contact.id)}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={() => removeContact(contact.id)}>
+                    <DeleteIcon />
+                  </IconButton>
                 </TableCell>
               </TableRow>
             ))}
